54f208311ad88654b51613c801e30c2b3bb34e6b
Jovana Ristic <jovana012002@gmail.com>
2025-06-04 22:39:48 +0200

    Outdoor cinema added

--- src/app/model/cinema-with-projections.ts
+++ src/app/model/cinema-with-projections.ts
@@ -3,5 +3,6 @@ import { ProjectionDTO } from './projection';
 export interface CinemaWithProjectionsDTO {
   cinemaId: number;
   cinemaName: string;
+  type: string;
   projections: ProjectionDTO[];
 }
--- src/app/outdoor-cinema/outdoor-cinema.component.ts
+++ src/app/outdoor-cinema/outdoor-cinema.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CinemaWithProjectionsDTO } from '../model/cinema-with-projections';
 
 @Component({
   selector: 'app-outdoor-cinema',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './outdoor-cinema.component.css'
 })
 export class OutdoorCinemaComponent {
+  @Input() cinema!: CinemaWithProjectionsDTO | undefined
+  rows = Array(3).fill(0);  // 4 parking rows
+  spots = Array(7).fill(0); // 6 spots per row
+  selectedSpot: { row: number, spot: number } | null = null;
+  price: number = 12.99; // or dynamic
 
+  selectSpot(row: number, spot: number) {
+    this.selectedSpot = { row, spot };
+  }
+
+  reserveSpot() {
+    if (this.selectedSpot) {
+      const seatNumber = 100 + this.selectedSpot.row * this.spots.length + this.selectedSpot.spot + 1;
+      console.log(`Reserving seat ${seatNumber}`); 
+    }
+  }
 }
