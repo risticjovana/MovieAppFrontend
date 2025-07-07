cf98cb673d5eaecea005ac60117bb93cd1207dcf
Jovana Ristic <jovana012002@gmail.com>
2025-07-07 20:18:22 +0200

    homepage slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +14,26 @@ export class HomeComponent implements OnInit {
   token: string | null = null;
   user: any = null;
 
+  movies: string[] = ['Ballerina', 'Spider-Man: Across the Spider-Verse', 'Blade Runner 2049'];
+  backdropUrls: { [movieName: string]: string } = {};
+
+
+  constructor(private movieService: MovieService) {}
+
+  ngAfterViewInit(): void {
+    new Swiper('.movie-slider', {
+      slidesPerView: 'auto',
+      spaceBetween: 40,
+      centeredSlides: true,
+      grabCursor: true,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+  }
+
   ngOnInit(): void {
     this.token = localStorage.getItem('token');
     
@@ -21,7 +44,20 @@ export class HomeComponent implements OnInit {
       } catch (error) {
         console.error('Invalid token', error);
       }
-    }
+    } 
+    
+    // Load backdrops for each movie
+    this.movies.forEach(movieName => {
+      this.movieService.getBackdrop(movieName).subscribe({
+        next: (backdropUrl) => {
+          this.backdropUrls[movieName] = backdropUrl;
+        },
+        error: () => {
+          console.warn(`Failed to load backdrop for ${movieName}, using default.`);
+          this.backdropUrls[movieName] = 'assets/default-backdrop.jpg';
+        }
+      });
+    });
   }
 
   toggleSidebar() {
